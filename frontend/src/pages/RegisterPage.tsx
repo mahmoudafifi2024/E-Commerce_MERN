80422199a02bb0188d9a35f8c0898dc289683d7e
Mahmoud Afifi <mahmoudafifi2024@gmail.com>
2025-02-14 06:09:45 +0200

    AuthContext + RegisterPage Integration

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,22 +1,31 @@
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import { useRef, useState } from "react";
 import { BASE_URL } from "../constants/baseUrl";
+import { useAuth } from "../context/Auth/AuthContext";
 
 
 export default function RegisterPage() {
+    // Error message
     const [error, setError] = useState("")
+    
     const firstNameRef = useRef<HTMLInputElement>(null);
     const lastNameRef = useRef<HTMLInputElement>(null);
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+
+    const {login} = useAuth()
     
     const onSubmit = async () => {
         const firstName = firstNameRef.current?.value;
         const lastName = lastNameRef.current?.value;
         const email = emailRef.current?.value; 
         const password = passwordRef.current?.value;  
-    
-        console.log({ firstName,lastName,  email, password });
+        
+        // Validate the form data
+        if(!firstName || !lastName || !email || !password){
+            setError("Check submited data")
+            return
+        }
 
         // Make the call to API to Create the user
         const response = await fetch(`${BASE_URL}/user/register`,{
@@ -37,8 +46,16 @@ export default function RegisterPage() {
             return
         }
 
-        const data = await response.json()
-        console.log(data)
+        const token = await response.json()
+
+        if(!token){
+            setError("Incorrect token")
+            return
+        }
+
+        login(email, token)
+
+        console.log(token)
     };
     
 return (
